d3fc9f58130161f5a2ea48850141f6a6f278b0b9
Christoph Anderson <CWolfAnderson@gmail.com>
2016-10-22 19:37:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,8 @@ let Alexa = require('alexa-sdk');
 let firebase = require('firebase');
 let config = require('./config');
 
-let APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-let SKILL_NAME = 'Space Facts';
-
-// Initialize Firebase
-// let config = {
-//   apiKey: "AIzaSyBYgd7-JPPqhAZT8udAC7BbcXqVXZ-nyJ8",
-//   authDomain: "echo-smarts.firebaseapp.com",
-//   databaseURL: "https://echo-smarts.firebaseio.com",
-//   storageBucket: "echo-smarts.appspot.com",
-//   messagingSenderId: "671614526969"
-// };
-// firebase.initializeApp(config);
+let APP_ID = undefined;
+let SKILL_NAME = 'Firebase';
 
 firebase.initializeApp({
   serviceAccount: config,
@@ -57,28 +47,13 @@ let handlers = {
     let today = new Date();
     let time = today.toLocaleString(); 
     
-    database.ref('facts').push({
+    database.ref().push({
       fact: randomFact,
       timestamp: time
     }, () => {
         this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)      
-    });  
-            
-    // database.ref().push('hello there');
-    
-    // database.ref('facts').on('value', function(snapshot) {
-    //   this.emit(':tellWithCard', 'snapshot.val().fact', SKILL_NAME, randomFact)      
-    // });
-    
-    // let adaRef = firebase.database().ref('facts');
-    // let path = adaRef.root.toString();
-    // path is now 'https://sample-app.firebaseio.com'
-    
-    // setTimeout(function() {
-    // this.emit(':tellWithCard', path, SKILL_NAME, randomFact)
-    // this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)      
-    // }, 2000);
-    
+    });
+
   },
   'AMAZON.HelpIntent': function () {
     let speechOutput = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
